2b572dbda91544740b0d3cdccb87b48dd948eeb1
Shantanu Patil <shantanu.patil@iongroup.com>
2019-08-22 16:03:16 +0530

    updated datafile

--- scrap/GlobalVars.py
+++ scrap/GlobalVars.py
@@ -2,13 +2,10 @@ USERNAME = "username"
 PASSWORD = "password"
 URL = "https://portal.tpt.com/processpoint/browse/CXL-"
 LOGIN_URL = "https://portal.tpt.com/processpoint/login.jsp"
-TICKET_START = 30200
+TICKET_START = 19
 TICKET_END = 61500
-# TICKET_START = 30199
-# TICKET_END = 30199
 COMMENT_SEP = "$$"
 COMMENT_START = "{{"
 COMMENT_END = "}}"
 FILENAME = "../resources/data.csv"
 
-# Entry for CXL-30199 inserted
