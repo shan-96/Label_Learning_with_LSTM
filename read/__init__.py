3b0c1e66b83aa47a609f4c17ba2246a064d6b77d
Shantanu Patil <shantanu.patil@iongroup.com>
2019-08-29 00:00:29 +0530

    added clean data file

--- read/__init__.py
+++ read/__init__.py
@@ -1,10 +1,12 @@
 import csv
+import sys
 
 from read.Cleaner import Cleaner
 from scrap import Writer
 from scrap.GlobalVars import DATAFILENAME, FILENAME
 
 if __name__ == "__main__":
+    csv.field_size_limit(sys.maxsize)
     cleaner = Cleaner()
     writer = Writer(DATAFILENAME)
 
@@ -12,9 +14,9 @@ if __name__ == "__main__":
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
             jira, comments, resolution = cleaner.explode_row(row)
-            if (jira != "jira"):
+            if jira != "jira":
                 comments = cleaner.getComments(comments)
                 comments = cleaner.clean(comments)
             writer.write(jira, comments, resolution, csv.QUOTE_NONE)
-            if (jira != "jira"):
+            if jira != "jira":
                 print("Entry inserted for CXL-" + jira)
