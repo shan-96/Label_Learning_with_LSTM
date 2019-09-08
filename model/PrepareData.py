ad69dd4a858db9fc1beae2733a5e775f19bb2d7b
shan-96 <shantanumh18ab3632@gmail.com>
2019-09-08 19:49:07 +0530

    prepared train test and label data

--- model/PrepareData.py
+++ model/PrepareData.py
@@ -8,21 +8,21 @@ from scrap.GlobalVars import TRAIN_CSV_FILE, LF_CSV_FILE, TEST_CSV_FILE
 def prepare_train_csv(main_file_df):
     split = int(len(main_file_df) * 0.8)
     sample_df = main_file_df.iloc[0:split]
-    sample_df = sample_df['resolution'].apply(lambda x: '')
-    sample_df.to_csv(path_or_buf=TRAIN_CSV_FILE, quoting=csv.QUOTE_NONE)
+    sample_df['resolution'] = sample_df['resolution'].apply(lambda x: '')
+    sample_df.to_csv(index=False, path_or_buf=TRAIN_CSV_FILE, quoting=csv.QUOTE_NONE)
 
 
 def prepare_test_csv(main_file_df):
     split1 = int(len(main_file_df) * 0.8)
     split2 = int(len(main_file_df) * 0.9)
     sample_df = main_file_df.iloc[split1:split2]
-    sample_df.to_csv(path_or_buf=TEST_CSV_FILE, quoting=csv.QUOTE_NONE)
+    sample_df.to_csv(index=False, path_or_buf=TEST_CSV_FILE, quoting=csv.QUOTE_NONE)
 
 
 def prepare_label_csv(main_file_df):
     split = int(len(main_file_df) * 0.1)
-    sample_df = main_file_df.iloc[-split]
-    sample_df.to_csv(path_or_buf=LF_CSV_FILE, quoting=csv.QUOTE_NONE)
+    sample_df = main_file_df.tail(split)
+    sample_df.to_csv(index=False, path_or_buf=LF_CSV_FILE, quoting=csv.QUOTE_NONE)
 
 
 class PrepareData:
@@ -35,7 +35,7 @@ class PrepareData:
         main_file_df = pd.read_csv(self.mainfile)
         fixed_list = ['Fixed', 'Done', 'Fixed', 'Fixed in a Later Version', 'Enhancement implemented',
                       'Training Provided', 'Working as Expected', 'Request Completed', 'Not an Issue']
-        main_file_df = main_file_df['resolution'].apply(lambda x: 1 if x in fixed_list else 0)
+        main_file_df['resolution'] = main_file_df['resolution'].apply(lambda x: 1 if x in fixed_list else 0)
 
         # large number of unlabelled tickets
         prepare_train_csv(main_file_df)
--- model/__init__.py
+++ model/__init__.py
@@ -1,9 +1,11 @@
 from model.CommentSnorkel import CommentSnorkel
-from model.PrepareData import PrepareData
-from scrap.GlobalVars import DATAFILENAME
+
+# from model.PrepareData import PrepareData
+# from scrap.GlobalVars import DATAFILENAME
 
 if __name__ == "__main__":
-    prepare = PrepareData(DATAFILENAME)
+    # prepare = PrepareData(DATAFILENAME)
+    # prepare.prepare_data()
     model_trainer = CommentSnorkel()
     trainer1, Y_Fits = model_trainer.getTrainedModel1()
     trainer2 = model_trainer.getTrainedModel2()
